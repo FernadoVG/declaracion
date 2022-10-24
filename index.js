663f4bd3934f37661be15e766d6daf159abf6343
FernadoVG <81638738+FernadoVG@users.noreply.github.com>
2022-10-23 22:32:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si ñam')
+    alert('Sabia que ibas a decir que si ñam (PSDT: Pimpon Zzzz)')
 });
 
 const noBtn = document.querySelector('#noBtn');
